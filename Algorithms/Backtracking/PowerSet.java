60b880315d18f3737eb1742d4b266422251baf93
Neel Patel <neel@Neels-MacBook-Pro.local>
2025-02-01 18:05:01 -0500

    minor change to power set algo

--- Algorithms/Backtracking/PowerSet.java
+++ Algorithms/Backtracking/PowerSet.java
@@ -6,24 +6,29 @@ import java.util.Stack;
 
 public class PowerSet {
 
+    private int[] nums;
+    private List<List<Integer>> res;
+    private Stack<Integer> curr;
+
     /*
      * Return all possible subsets of a list of numbers
      * Runtime: O(2^n), n is the size of the list
      */
 
-    public static List<List<Integer>> powerSet(int[] nums){
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        Stack<Integer> current = new Stack<Integer>();
+    public List<List<Integer>> powerSet(int[] nums){
+        this.nums = nums;
+        this.res = new ArrayList<List<Integer>>();
+        this.curr = new Stack<Integer>();
 
-        backtracking(0, current, result, nums);
-        return result;
+        backtracking(0);
+        return res;
     }
 
     /*
      * Decision tree solution
      * Decision space: add and don't add
      */
-    private static void backtracking(int i, Stack<Integer> curr, List<List<Integer>> res, int[] nums){
+    private void backtracking(int i){
         if(i == nums.length){
             res.add(new ArrayList<>(curr));
             return;
@@ -31,11 +36,11 @@ public class PowerSet {
 
         // Add
         curr.push(nums[i]);
-        backtracking(i+1, curr, res, nums);
+        backtracking(i+1);
 
         // Don't add
         curr.pop();
-        backtracking(i+1, curr, res, nums);
+        backtracking(i+1);
 
     }
 
@@ -53,10 +58,9 @@ public class PowerSet {
      */
 
     public static void main(String[] args){
-        // int[] nums = {3,6,9};
-        int[] nums = {2,4};
-        System.out.println(powerSet(nums));
-
+        int[] nums = {3,6,9};
+        // int[] nums = {2,4};
+        PowerSet ps = new PowerSet();
+        System.out.println(ps.powerSet(nums));
     }
-    
 }
